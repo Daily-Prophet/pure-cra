eed6254cd2fdb4c88dbf1194d579653521d0c6d6
付洋朕 <fuyangzhen@fuyangzhendeAir.lan>
2020-12-27 22:09:57 +0800

    deployment

--- src/server/server.js
+++ src/server/server.js
@@ -22,8 +22,11 @@ authenticationRoute(app);
 
 if (process.env.NODE_ENV === `production`) {
   app.use(express.static(path.resolve(__dirname, 'build')));
-  app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build/index.html'));
+  // app.get('/*', (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, 'build/index.html'));
+  // })
+  app.get('/', (req, res) => {
+    res.send('hello world');
   })
 }
 
