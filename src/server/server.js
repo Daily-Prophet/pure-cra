5ac7915f8e64740130c624d957a8cb11745ad0f0
付洋朕 <fuyangzhen@fuyangzhendeAir.lan>
2020-12-27 20:19:30 +0800

    deployment

--- src/server/server.js
+++ src/server/server.js
@@ -20,11 +20,10 @@ app.use(
 
 authenticationRoute(app);
 
-
 if (process.env.NODE_ENV === `production`) {
-  app.use(express.static(path.resolve(__dirname, '../build/dist')));
+  app.use(express.static(path.resolve(__dirname, 'build')));
   app.get('/*', (req, res) => {
-    res.sendFile(path.resolve('index.html'));
+    res.sendFile(path.resolve(__dirname, 'build/index.html'));
   })
 }
 
